076c48d53bfc7252e2d16d9f2488de47d4007030
ibo321 <iboo96_96@hotmail.com>
2018-05-23 16:50:33 +0200

    Add project files.

---
+++ CrossApp/CrossApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void Button_OnPressed(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Display dialog", "Det virker", "CLOSE");
+	    }
+	}
+}
